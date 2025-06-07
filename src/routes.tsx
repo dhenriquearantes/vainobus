3a1a8642d5e5d50fa8366e08b34cfbd4c695c7ad
Divino H. Arantes <dhenriquearantes@gmail.com>
2025-06-07 15:05:33 -0300

    feat: adiciona rotas para registro e recuperação de senha

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { LoginPage } from './pages/Login';
 import { DashboardPage } from './pages/Dashboard';
+import { RecoverPasswordPage } from './pages/RecoverPassword';
+import { RegisterPage } from './pages/Register';
 
 export const router = createBrowserRouter([
   {
@@ -10,5 +12,13 @@ export const router = createBrowserRouter([
   {
     path: '/dashboard',
     element: <DashboardPage />,
+  },
+  {
+    path: '/recuperar-senha',
+    element: <RecoverPasswordPage />,
+  },
+  {
+    path: '/cadastro',
+    element: <RegisterPage />,
   }
 ]); 
