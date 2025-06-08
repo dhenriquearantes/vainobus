3bcea64ddefc71f6770cc411bf0e115f75154231
Divino H. Arantes <dhenriquearantes@gmail.com>
2025-06-08 01:35:27 -0300

    feat: adiciona função de registro de usuário na API

---
+++ src/api/register.ts
@@ -0,0 +1,36 @@
+import { api } from '@/lib/axios'
+
+export interface RegisterRequest {
+  name: string
+  email: string
+  cpf: string
+  password: string
+}
+
+export interface RegisterResponse {
+  data: {
+    user: {
+      id: string
+      name: string
+      email: string
+      cpf: string
+    }
+    token: string
+  }
+}
+
+export async function register({
+  name,
+  email,
+  cpf,
+  password,
+}: RegisterRequest): Promise<RegisterResponse> {
+  const response = await api.post('/user/register', {
+    name,
+    email,
+    cpf,
+    password,
+  })
+
+  return response.data
+} 
